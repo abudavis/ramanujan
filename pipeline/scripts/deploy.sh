9593b6b56adc28a95fff9c4245744bfea1ee8f20
abudavis <abudavis@gmail.com>
2020-03-24 18:17:46 +0100

    updated with er

--- pipeline/scripts/deploy.sh
+++ pipeline/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # NAME: deploy.sh
-# Usage: ./deploy.sh
+# Usage: ./deploy.sh <pwd> <repo-name>
 # INITIAL CREATION DATE:	March 18, 2020
 # lAST MODIFIED DATE:	March 22, 2020
 # AUTHOR:   Abu Davis, www.integrationpattern.com
@@ -9,7 +9,7 @@
 # 	This script deploys helm chart for specified product in IBM CP4I, the container image is pulled from the internal OCP image registry
 
 if [ -z $1 ] ; then
-	echo "Usage: ./deploy.sh "
+	echo "Usage: ./deploy.sh <pwd> <repo-name>"
   exit 1
 else
   PASS=$1
@@ -19,14 +19,12 @@ else
 fi
 
 RELEASENAME="dummy-ds-rel-java"
-#tip007-ds-transics-java
+#dummy-ds-rel-java
 CPULIMIT="100m"
 ACEHELMCHART="https://raw.githubusercontent.com/IBM/charts/master/repo/entitled/ibm-ace-server-icp4i-prod-3.0.0.tgz"
 ISPROD="false"
 
-#cp.icr.io/ibm-ace-mqclient-server-prod:11.0.0.6.1
-IMAGEPULLSECRET="imageregistrysecret"
-#ibm-entitlement-key
+IMAGEPULLSECRET="deployer-dockercfg-7vxwq"
 
 NAMESPACE="ace"
 #PASS=$(curl -ks https://testengine.tine.no/qa/internet/postenpnrlookup/?postnummer=3001);
@@ -35,11 +33,13 @@ CLOUDCTLHOST="icp-console.cpi.testcluster.ocp.tine.no"
 ACEIMAGETYPE="acemqclient"
 
 #Replace with image created by Jenkins
-ACEMQCLIENTIMAGE="ace-app-amd64"
+#ACEMQCLIENTIMAGE="ace-app-amd64"
+#ace-app-amd64
 #helloworld-amd64
 
 #Remove the appended "-amd64" at end of image as IBM helm chart inserts it at the end of the image during helm install.
-ACEMQCLIENTIMAGETMP=$(echo $ACEMQCLIENTIMAGE | sed 's/-amd64//g');
+#ACEMQCLIENTIMAGETMP=$(echo $ACEMQCLIENTIMAGE | sed 's/-amd64//g');
+ACEMQCLIENTIMAGETMP=$RELEASENAME;
 ACEMQCLIENTIMAGEURL="image-registry.openshift-image-registry.svc:5000/$NAMESPACE/$ACEMQCLIENTIMAGETMP"
 echo "---"
 echo "ACE modified baked image url: $ACEMQCLIENTIMAGEURL"
@@ -65,7 +65,8 @@ echo "---"
 echo "Installing Helm Chart for ACE..."
 echo "---"
 linux-amd64/helm install --namespace ace --name $RELEASENAME $ACEHELMCHART?raw=true --set imageType=$ACEIMAGETYPE --set productionDeployment=$ISPROD --set image.acemqclient=$ACEMQCLIENTIMAGEURL --set image.pullSecret=$IMAGEPULLSECRET --set persistence.enabled=false --set persistence.useDynamicProvisioning=false --set aceonly.resources.requests.cpu=25m --set aceonly.resources.limits.cpu=$CPULIMIT --set aceonly.replicaCount=1 --set odTracingConfig.enabled=true --set odTracingConfig.odTracingNamespace=integration --set license=accept --debug --tls
+echo "Adding route for the ace server pod"
+oc expose svc $RELEASENAME-ibm-ace-server-icp4i-prod --port=7800
 echo "---"
 echo "Finished Script"
 echo "---"
-
